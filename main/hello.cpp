0331a3294eefe7009f8283671f9d21cc7523b3a4
Shivadarshan Devadiga <shivadarshan2212@gmail.com>
2025-03-13 16:39:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG22CS560 Shiva\n";
+  cout << "PES1UG22CS560\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
